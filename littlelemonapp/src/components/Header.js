4d9cfac4ac31ca47e06ef414c0af061a48461d46
Christopher Chen <chrischen0127@gmail.com>
2023-02-10 20:09:04 +0800

    added more components

--- littlelemonapp/src/components/Header.js
+++ littlelemonapp/src/components/Header.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useState} from 'react';
+import "../components/styles/Header.css"
+import Logo from "../components/assets/logo.jpg"
+import { NavLink } from 'react-router-dom';
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+
 
 const Header = () => {
-    return (
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to React</h1>
-      </header>
-    );
-  };
 
-export default Header;
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+
+  return (
+    <>
+      <header className="header-container">
+        <NavLink to = "/">
+            {<img className="logo-img" src = { Logo } alt = "Little Lemon logo" />}
+        </NavLink>
+        <nav>
+            <ul className={click ? "nav-menu active" : "nav-menu"}>
+                <li><NavLink to="/">Home</NavLink></li>
+                <li><NavLink to="#">About</NavLink></li>
+                <li><NavLink to="#">Menu</NavLink></li>
+                <li><NavLink to="/reservations">Reservations</NavLink></li>
+                <li><NavLink to="#">Order Online</NavLink></li>
+                <li><NavLink to="#">Login</NavLink></li>
+            </ul>
+        </nav>
+        <div className="hamburger" onClick = {handleClick}>
+            {click ? (<AiOutlineClose size={20} style={{ color: "#333333"}}/>) : (<AiOutlineMenu size={30} style={{ color: "#333333", borderLeft: "1px solid #333333", paddingLeft: "10px", height: "20px"}}/>)}  
+        </div>
+      </header>
+    </>
+  )
+}
+
+export default Header
